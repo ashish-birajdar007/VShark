c427275587acbb6db12c66f90e4f9e57c91a9ba2
Rushikesh Gaikwad <grushikesh907@gmail.com>
2022-06-19 15:45:51 +0530

    ActionBar removed

--- app/src/main/java/com/syndicate/vshark/CreateMeetActivity.java
+++ app/src/main/java/com/syndicate/vshark/CreateMeetActivity.java
@@ -2,11 +2,18 @@ package com.syndicate.vshark;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.jitsi.meet.sdk.JitsiMeet;
 import org.jitsi.meet.sdk.JitsiMeetActivity;
@@ -19,6 +26,7 @@ public class CreateMeetActivity extends AppCompatActivity {
 
     private TextView meetingCode;
     private String generatedMeetCode;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +35,10 @@ public class CreateMeetActivity extends AppCompatActivity {
 
         Button joinBtn = findViewById(R.id.join_btn2);
         Button shareBtn = findViewById(R.id.share_code_btn);
+        ImageView share = findViewById(R.id.share_app1);
+        ImageView options = findViewById(R.id.more_options1);
         meetingCode = findViewById(R.id.meeting_code);
+        mAuth = FirebaseAuth.getInstance();
         generatedMeetCode = getMeetingCode();
         meetingCode.setText(generatedMeetCode);
 
@@ -42,6 +53,30 @@ public class CreateMeetActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 shareMeetCode(generatedMeetCode);
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareApp();
+            }
+        });
+
+        options.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu menu = new PopupMenu(CreateMeetActivity.this, options);
+                menu.getMenuInflater().inflate(R.menu.menu_main, menu.getMenu());
+
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        optionsItemSelected(menuItem);
+                        return true;
+                    }
+                });
+                menu.show();
             }
         });
 
@@ -74,6 +109,37 @@ public class CreateMeetActivity extends AppCompatActivity {
         return meetCode.toString();
     }
 
+    public void optionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.idLogOut:
+                Toast.makeText(this, "Logged Out", Toast.LENGTH_SHORT).show();
+                mAuth.signOut();
+                Intent i = new Intent(CreateMeetActivity.this, LoginActivity.class);
+                startActivity(i);
+                this.finish();
+                break;
+            case R.id.addUserProfile:
+                startActivity(new Intent(CreateMeetActivity.this, AddUserProfileActivity.class));
+                break;
+            case R.id.userProfile:
+                startActivity(new Intent(CreateMeetActivity.this, UserProfileActivity.class));
+                break;
+            case R.id.viewPitcherProfile:
+                startActivity(new Intent(CreateMeetActivity.this, viewPitcherProfile.class));
+                break;
+        }
+    }
+
+    private void shareApp() {
+        Intent ip = new Intent(Intent.ACTION_SEND);
+        ip.setType("text/plain");
+        ip.putExtra(Intent.EXTRA_SUBJECT,"Share vShark");
+        String share_Url="https://vshark220906537.wordpress.com/";
+        ip.putExtra(Intent.EXTRA_TEXT,share_Url);
+        startActivity(Intent.createChooser(ip,"Share Using"));
+    }
+
     private void shareMeetCode(String code)
     {
         Intent sendIntent = new Intent();
@@ -84,4 +150,5 @@ public class CreateMeetActivity extends AppCompatActivity {
         Intent shareIntent = Intent.createChooser(sendIntent, "Share Notes");
         startActivity(shareIntent);
     }
+
 }
--- app/src/main/java/com/syndicate/vshark/MainActivity.java
+++ app/src/main/java/com/syndicate/vshark/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -31,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
         Button createMeetBtn = findViewById(R.id.create_meet_btn);
         Button joinMeetBtn = findViewById(R.id.join_btn1);
         EditText meetingCode = findViewById(R.id.meeting_code_box);
+        ImageView share = findViewById(R.id.share_app);
+        ImageView options = findViewById(R.id.more_options);
+
         URL serverURL = null;
 
 
@@ -60,16 +65,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareApp();
+            }
+        });
+
+        options.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu menu = new PopupMenu(MainActivity.this, options);
+                menu.getMenuInflater().inflate(R.menu.menu_main, menu.getMenu());
+
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        optionsItemSelected(menuItem);
+                        return true;
+                    }
+                });
+                menu.show();
+            }
+        });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    private void optionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         switch (id){
             case R.id.idLogOut:
@@ -78,25 +101,25 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(i);
                 this.finish();
-                return true;
+                break;
             case R.id.addUserProfile:
                 startActivity(new Intent(MainActivity.this, AddUserProfileActivity.class));
-                return true;
+                break;
             case R.id.userProfile:
                 startActivity(new Intent(MainActivity.this, UserProfileActivity.class));
-                return true;
+                break;
             case R.id.viewPitcherProfile:
                 startActivity(new Intent(MainActivity.this, viewPitcherProfile.class));
-                return true;
-            case R.id.share:
-                Intent ip = new Intent(Intent.ACTION_SEND);
-                ip.setType("text/plain");
-                ip.putExtra(Intent.EXTRA_SUBJECT,"Share vShark");
-                String share_Url="https://vshark220906537.wordpress.com/";
-                ip.putExtra(Intent.EXTRA_TEXT,share_Url);
-                startActivity(Intent.createChooser(ip,"Share Using"));
-            default:
-                return super.onOptionsItemSelected(item);
+                break;
         }
+    }
+
+    private void shareApp() {
+        Intent ip = new Intent(Intent.ACTION_SEND);
+        ip.setType("text/plain");
+        ip.putExtra(Intent.EXTRA_SUBJECT,"Share vShark");
+        String share_Url="https://vshark220906537.wordpress.com/";
+        ip.putExtra(Intent.EXTRA_TEXT,share_Url);
+        startActivity(Intent.createChooser(ip,"Share Using"));
     }
 }
